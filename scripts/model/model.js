c4bd8d12d253c15c6dd62d69155045c3ebe63bf2
Alexander Stone <aloecend@gmail.com>
2018-05-01 09:01:39 -0700

    added basic UNTESTED support for sorting and rendering sections.

--- scripts/model/model.js
+++ scripts/model/model.js
@@ -1,1 +1,30 @@
 var app = app || {};
+
+//TODO: Test the below functions
+(function(model) {
+  function Section(){
+    this.order = 0;
+    this.html = '';
+  }
+
+  Section.all = [];
+
+  Section.renderAll = function(startElement) {
+    Section.sortAll();
+    Section.all.forEach(e => {
+      e.render(startElement)
+      startElement = e;
+    })
+  }
+
+  Section.prototype.render = function (previousElement){
+    previousElement.after(this.html);
+  }
+
+  module.Section = Section;
+
+  Section.sortAll = function() {
+    Section.all.sort( (a,b) => a-b);
+  }
+
+})(app);
