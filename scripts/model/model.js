b457489ac4fd025e12e6a85bc11896744503439c
MADELINE PETERS <MADELINEPET@HOTMAIL.COM>
2018-05-01 10:21:19 -0700

    We finished testing the modules functionality. Alex navigated, Madi drove.

--- scripts/model/model.js
+++ scripts/model/model.js
@@ -8,8 +8,9 @@ var app = app || {};
   }
 
   Section.all = [];
-
+  //startElement must be a jQuery element
   Section.renderAll = function(startElement) {
+    $(startElement).empty();
     Section.sortAll();
     Section.all.forEach(e => {
       $(startElement).append(e.render());
@@ -25,7 +26,7 @@ var app = app || {};
   module.Section = Section;
 
   Section.sortAll = function() {
-    Section.all.sort( (a,b) => a-b);
+    Section.all.sort( (a,b) => a.order-b.order);
   }
 
 })(app);
