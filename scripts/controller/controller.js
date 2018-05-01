04649549118951e271049b94874502445dfc40b1
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-05-01 12:46:00 -0700

    added AJAX calls in controller.js. NOT DONE

--- scripts/controller/controller.js
+++ scripts/controller/controller.js
@@ -5,6 +5,7 @@ ENV.apiUrl = 'https://fozzie-web-builder.herokuapp.com';
 
 var app = app || {};
 
+//proof of life function
 function Test() {
     $.get(`${ENV.apiUrl}/users`)
         .then(results => {
@@ -13,6 +14,28 @@ function Test() {
     // .catch(console.error());
 }
 
+// updating the project
+function updateProject(project_id) {
+    Section.all.forEach(e => htmlArr.push(e.body));
+    let htmlStr = JSON.stringify(htmlArr);
+    $.ajax({
+        url: `/app/data/${project_id}`,
+        method: 'PUT',
+        data: { html: htmlStr }
+    })
+        .then(callback);
+}
+
+// getting the info of a specifically selected project from the project page
+function getProject() {
+    $.get(`/app/data/${project}`)
+        .then(results => {
+            let htmlArr = JSON.parse(results.html);
+            let Section.all = htmlArr.map((e, i) => {
+                return new Section(i, e)
+            })
+        })
+}
 Test();
 // function testCallback(results) {
 //     $('p').text(results);
