2ba06b350091360dc5d96c253ec8ea5c6a170817
Justin Morris <justinjodymorris@gmail.com>
2018-05-02 16:19:36 -0700

    added styling for user web builder

--- scripts/view/create-view.js
+++ scripts/view/create-view.js
@@ -1,15 +1,54 @@
 'use strict';
 
+const defaultBody = `<section class="cols-1 template-body"><article><img src="./assets/img/fozzie-web-builder-logo.png" alt="img alt"></article></section>`;
+
 var app = app || {};
 
 (function (module) {
     var createView = {};
 
-    createView.initCreateView = function () {
-        $('everything else').hide();
-        $('#create-view').show();
+    var positionCounter = project.allSections.length - 1;
+
+    $('#add-new-section').on('click', createSection);
+
+    $('#user-input-menu li').on('click', assignTemplate);
+
+    function assignTemplate() {
+        let templateNum = $(this).data('cols');
+        let body = project.allSections[positionCounter].body
+        body = app.templates.templateToHtml(templateNum, body);
+
+        project.allSections[positionCounter].body = body;
+        project.renderAll($('#web-row-container'));
+
+        project.updateProject();
+        
+        $('#user-input-menu').css('left', '-33%');
     }
 
+    function createSection() {
+        positionCounter++;
+        let section = new app.Section(positionCounter, defaultBody);
 
+        project.allSections.push(section);
+        project.renderAll($('#web-row-container'));
+
+        $('#user-input-menu').css('left', '0');
+    }
+
+    function initWebBuilderViewToHtml() {
+        var source = document.getElementById("web-builder-article-template").innerHTML;
+        var template = Handlebars.compile(source);
+        return template;
+    };
+
+    createView.initCreateView = function() {
+        $('section').hide();
+        $('section#web-builder-view').show();
+        $('section#web-builder-view').children().show();
+        $('body').css('background', '#ffffff');
+    } 
+
+    module.createView = createView;
 
 })(app);
--- scripts/view/view.js
+++ scripts/view/view.js
@@ -1,52 +1,12 @@
 'use strict';
 
 //TODO: Making Default Body
-const defaultBody = `<section class="cols-1"><article><img src='http://via.placeholder.com/480x350' alt="img alt"></article></section>`;
 
 //TODO: make selection class that highlights section you are working on - add and remove class to users currently selected thing
 var app = app || {};
 
 (function(module) {
-  var webBuilderView = {};
-
-  var positionCounter = project.allSections.length - 1;
-
-  $('#add-new-section').on('click', createSection);
-
-  $('#user-input-menu li').on('click', assignTemplate);
-
-  function assignTemplate() {
-    let templateNum = $(this).data('cols');
-    let body = project.allSections[positionCounter].body
-    body = app.templates.templateToHtml(templateNum, body);
-
-    project.allSections[positionCounter].body = body;
-    project.renderAll($('#web-row-container'));
-
-    project.updateProject();
-    $('#user-input-menu').css('left', '-33%');
-  }
-
-  function createSection() {
-    positionCounter++;
-    let section = new app.Section(positionCounter, defaultBody);
-
-    project.allSections.push(section);
-    project.renderAll($('#web-row-container'));
-
-    $('#user-input-menu').css('left', '0');
-  }
-
-  function initWebBuilderViewToHtml() {
-    var source = document.getElementById("web-builder-article-template").innerHTML;
-    var template = Handlebars.compile(source);
-    return template;
-  };
-
-  function initWebBuilderView() {
-    $('section').hide();
-    $('section#web-builder-view').show();
-  } 
+  
 
 })(app)
 
