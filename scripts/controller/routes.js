16ed538ef27165cdb890376517cd49e7d27fc4ca
Justin Morris <justinjodymorris@gmail.com>
2018-05-04 12:00:01 -0700

    Update routes.js

--- scripts/controller/routes.js
+++ scripts/controller/routes.js
@@ -1,4 +1,5 @@
 page("/", () => app.loginView.initLoginView());
+page("/Fozzie-s-Website-Builder-Client", () => app.loginView.initLoginView());
 page("/sign-up", () => app.homeView.initHomeView());
 page("/create/:project_id/:project_name", ctx => app.createView.initCreateView(ctx));
 page("/create/new/:user_id", (ctx) => app.createView.initCreateView(ctx));
